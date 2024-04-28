2a487fbc0ffc24c1a62984410659227db8490ab9
Kody Baker <132102928+kbaker1212@users.noreply.github.com>
2024-04-28 01:04:07 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,10 +13,13 @@ module "security" {
 # Import the compute module (EC2 and Auto Scaling Group)
 module "compute" {
   source        = "./modules/compute"  # Path to the compute module
-  redhat_ami    = "ami-XXXXXXXX"       # Replace with the Red Hat AMI ID
+  redhat_ami    = "ami-0fe630eb857a6ec83"       # Replace with the Red Hat AMI ID
   sub2_id       = module.vpc.sub2_id   # Public subnet ID for the EC2 instance
   sub3_id       = module.vpc.sub3_id   # Private subnet IDs for ASG
+  sub4_id       = module.vpc.sub4_id   # Private subnet IDs for ASG
   public_sg_id  = module.security.public_sg_id  # Security group ID for public instances
+  key_name      = aws_key_pair.my_new_key_pair.key_name  # Reference the key pair
+
 }
 
 # Import the Application Load Balancer module
@@ -24,6 +27,7 @@ module "alb" {
   source           = "./modules/alb"  # Path to the ALB module
   vpc_id           = module.vpc.vpc_id  # Pass the VPC ID
   sub3_id          = module.vpc.sub3_id  # Private subnet IDs for ALB
+  sub4_id          = module.vpc.sub4_id  # Reference to Subnet 4
   target_group_arn = module.compute.asg_name  # Target group for ALB
 }
 
@@ -32,4 +36,3 @@ module "s3" {
   source      = "./modules/s3"  # Path to the S3 storage module
   bucket_name = "9911techpoc"  # Set your S3 bucket name
 }
-
